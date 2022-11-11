08578e2cb581f00579e2a0b86a2ca3fde1c293e7
Batyanoskow <Lyubchenko2910@gmail.com>
2022-11-11 10:18:48 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header">
+        <nav class="navbar navbar-light bg-dark">
+            <div class="container">
+                 <a class="navbar-brand" href="#">
+                    <img src="../assets/logo.jpeg" alt="" width="60" height="60">
+                 </a>
+                 <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="/">Home</a>
+                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="/create">CreateToDo</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>                                                                                         
+<script>
+export default {
+    name : "HeaderVue",
+}
+</script>
+<style lang="scss">
+    
+</style>
